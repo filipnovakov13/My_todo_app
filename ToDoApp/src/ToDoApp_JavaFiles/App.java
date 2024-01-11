329d62eb6a6d5bf3da5e26c2ba4748e02c7e4e13
Filip Novakov <filip.novakov13@gmail.com>
2024-01-11 16:42:57 +0100

    Added functionality for checking and removing tasks, as well as argument error handling

--- ToDoApp/src/ToDoApp_JavaFiles/App.java
+++ ToDoApp/src/ToDoApp_JavaFiles/App.java
@@ -1,19 +1,35 @@
 package ToDoApp_JavaFiles;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class App {
 
     public static void main(String[] args) throws IOException {
 
         System.out.println(Arrays.toString(args));
+
+        String[] commands = {"-l", "-a", "-c", "-r"};
+
+        if (!Arrays.asList(commands).contains(args[0])){
+            System.err.println("Unsupported argument");
+
+            System.out.println("Command Line Todo application\n" +
+                    "=============================\n" +
+                    "\n" +
+                    "Command line arguments:\n" +
+                    "    -l   Lists all the tasks\n" +
+                    "    -a   Adds a new task\n" +
+                    "    -r   Removes a task\n" +
+                    "    -c   Completes a task");
+            return;
+        }
 
         if (args.length == 0) {
             System.out.println("Command Line Todo application\n" +
@@ -28,7 +44,7 @@ public class App {
         }
 
         if (args[0].contentEquals("-l")) {
-            try(BufferedReader reader = new BufferedReader(new FileReader("src/ToDoList/TaskList.txt"))) {
+            try (BufferedReader reader = new BufferedReader(new FileReader("src/ToDoList/TaskList.txt"))) {
                 String line;
                 int lineNumber = 1;
                 if (reader.readLine() != null) {
@@ -53,10 +69,11 @@ public class App {
                 String lineToAppend = args[1];
 
                 try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(filePath), StandardOpenOption.APPEND)) {
-                    writer.write(lineToAppend);
                     writer.newLine();
+                    writer.write(lineToAppend);
                 } catch (IOException e) {
-                    System.err.println("Unable to add the task.");;
+                    System.err.println("Unable to add the task.");
+                    ;
                 }
             }
         }
@@ -65,8 +82,70 @@ public class App {
             if (args.length == 1) {
                 System.out.println("Unable to check: no index provided");
             }
+
+            try {
+                int taskNumber = Integer.parseInt(args[1]);
+                String filePath = "src/ToDoList/TaskList.txt";
+
+                BufferedReader reader = new BufferedReader(new FileReader(filePath));
+                List<String> tasks = reader.lines().collect(Collectors.toList());
+
+                if (taskNumber >= 1 && taskNumber <= tasks.size()) {
+                    int index = taskNumber - 1;
+                    String taskToCheck = tasks.get(index);
+
+                    if (!taskToCheck.startsWith("[X] ")) {
+                        tasks.set(index, "[X] " + taskToCheck);
+
+                        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                            for (String task : tasks) {
+                                writer.write(task);
+                                writer.newLine();
+                            }
+                        }
+                    } else {
+                        System.out.println("Task " + taskNumber + " is already marked as completed.");
+                    }
+                } else {
+                    System.out.println("Unable to check: index is out of bound");
+                }
+            } catch (NumberFormatException e) {
+                System.err.println("Unable to check: index is not a number");
+            } catch (IOException e) {
+                System.err.println("Error reading/writing file");
+            }
         }
 
+        if (args[0].contentEquals("-r")) {
+            if (args.length < 2) {
+                    System.out.println("Unable to remove: no index provided");
+                throw new ArrayIndexOutOfBoundsException("");
+            }
 
+            try {
+                int taskNumber = Integer.parseInt(args[1]);
+                String filePath = "src/ToDoList/TaskList.txt";
+
+                BufferedReader reader = new BufferedReader(new FileReader(filePath));
+                List<String> tasks = reader.lines().collect(Collectors.toList());
+
+                if (taskNumber >= 1 && tasks.size() >= taskNumber) {
+                    tasks.remove(taskNumber - 1);
+                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                        for (String task : tasks) {
+                            writer.write(task);
+                            writer.newLine();
+                        }
+                    }
+                } else {
+                    System.out.println("Unable to remove: index is out of bound");
+                }
+            } catch (NumberFormatException e) {
+                System.err.println("Unable to remove: index is not a number");
+            } catch (IOException e) {
+                System.err.println("Error reading/writing file");
+            }
+        }
     }
 }
+
